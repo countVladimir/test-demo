59f8ad2d344c4e0275518614ada4bcc7df699aba
countVladimir <martin.ilievski00@gmail.com>
2020-08-19 13:41:02 +0200

    (un)changed controller

--- src/test/java/com/nnj/testdemo/rest/UserControllerTest.java
+++ src/test/java/com/nnj/testdemo/rest/UserControllerTest.java
@@ -12,11 +12,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.core.Is.is;
 
 @ExtendWith(MockitoExtension.class)
 class UserControllerTest {
